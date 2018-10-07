9e59822226009fcb9eda8367577383270e6477b7
Michel Araujo <araujo_michel@yahoo.com>
2018-10-07 19:33:50 -0300

    Made connection with database

--- apiTestLab.go
+++ apiTestLab.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,16 +8,15 @@ import (
 	"os"
 
 	"apiTestLab/model/business"
+	"apiTestLab/model/connection"
 	"apiTestLab/model/entities"
 
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const PATH_FILE_DB = "./db/database.db"
-
 func main() {
-	_, err := os.Stat(PATH_FILE_DB)
+	_, err := os.Stat(connection.PATH_FILE_DB)
 	if err != nil && os.IsNotExist(err) {
 		install()
 	}
@@ -40,12 +38,21 @@ func main() {
  * create the DB
  */
 func install() {
-	fmt.Println("Create database...")
-	database, _ := sql.Open("sqlite3", PATH_FILE_DB)
+	fmt.Println("## Create database")
+	database := connection.SqliteConnect()
 
-	createTableAuthors := "CREATE TABLE IF NOT EXISTS Authors (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, pass, TEXT)"
+	fmt.Println("## Create table")
+	createTableAuthors := "CREATE TABLE IF NOT EXISTS Author (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, pass, TEXT)"
 	statement, _ := database.Prepare(createTableAuthors)
 	statement.Exec()
+	statement.Close()
+
+	fmt.Println("## Insert data in table")
+	statement, _ = database.Prepare("INSERT INTO Author (name, email, pass) VALUES (?, ?, ?)")
+	statement.Exec("Author 01", "author01@teste.com", "1231")
+	statement.Exec("Author 02", "author02@teste.com", "1232")
+	statement.Exec("Author 03", "author03@teste.com", "1233")
+	statement.Close()
 }
 
 func getAuthors(responseWriter http.ResponseWriter, responseRead *http.Request) {
---
+++ model/connection/sqlite.go
@@ -0,0 +1,20 @@
+package connection
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const PATH_FILE_DB = "./db/database.db"
+
+var connectionDb *sql.DB
+
+// SqliteConnect return connection with db
+func SqliteConnect() *sql.DB {
+	if connectionDb == nil {
+		fmt.Println("Connect in DB")
+		connectionDb, _ = sql.Open("sqlite3", PATH_FILE_DB)
+		return connectionDb
+	}
+	return connectionDb
+}
